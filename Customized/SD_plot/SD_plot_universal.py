5b010e285c65f3afce973c7550a0c080299b8454
Xingrui Song <duxinyi@wustl.edu>
2023-09-11 03:37:34 -0500

    update

--- Customized/SD_plot/SD_plot_universal.py
+++ Customized/SD_plot/SD_plot_universal.py
@@ -16,6 +16,7 @@ import scipy
 from scipy.optimize import curve_fit
 
 import pickle
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(script_dir)
 parent_parent_dir = os.path.dirname(parent_dir)
@@ -30,7 +31,7 @@ from SD_Func import *
 """ To use this Plotter, below is an example: Example .py
 import sys
 import numpy as np
-ploter_path = r'C:\Users\Henriksen Lab\Documents\GitHub\Simple_DAQ\Customized\SD_plot'
+ploter_path = r"C:\\Users\\Henriksen Lab\\Documents\\GitHub\\Simple_DAQ\\Customized\\SD_plot"
 # note, the path of '\Simple_DAQ\Customized\SD_plot' on each local PC might be different, check the path before usage
 if ploter_path not in sys.path:
     sys.path.append(ploter_path)
@@ -50,8 +51,10 @@ plot_double_sweep(data,
     inside_plot_flag = True)
 """
 
-folder_path =''
-def plot_fig(note='', inside_plot_flag=True, timestamp= None):
+folder_path = ''
+
+
+def plot_fig(note='', inside_plot_flag=True, timestamp=None):
     if inside_plot_flag:
         print('Inside func plot')
         if timestamp is None:
@@ -70,14 +73,17 @@ def filter_nan(x, y):
 
 
 """------------------Plot configs-----------------------------------"""
-def plot_single_sweep(data, sweep_tag_1, plot_tag_x='VNA_freqs', plot_tag_y='VNA_log_mag', avgtype=None, yerrbar=False, inside_plot_flag=True, timestamp=None):
+
+
+def plot_single_sweep(data, sweep_tag_1, plot_tag_x='VNA_freqs', plot_tag_y='VNA_log_mag', avgtype=None, yerrbar=False,
+                      inside_plot_flag=True, timestamp=None):
     if timestamp is None:
         if 'timestamp' in data.keys():
             timestamp = min(data['timestamp'])
     # filter mask for single sweep
     sweep1 = data[sweep_tag_1]
-    sweep_1 = get_sweep(data,sweep_tag_1)
-    sweep_1_info = get_sweep_info(data,sweep_tag_1)
+    sweep_1 = get_sweep(data, sweep_tag_1)
+    sweep_1_info = get_sweep_info(data, sweep_tag_1)
     # Plot
     if inside_plot_flag:
         fg = plt.figure(figsize=fig_size, dpi=300)
@@ -89,7 +95,7 @@ def plot_single_sweep(data, sweep_tag_1, plot_tag_x='VNA_freqs', plot_tag_y='VNA
         xx = data[plot_tag_x][current_mask]
         yy = data[plot_tag_y][current_mask]
         x += [calc_average(xx)]
-        y += [calc_average(yy,type=avgtype)]
+        y += [calc_average(yy, type=avgtype)]
         yerr += [np.std(yy)]
     plt.xlabel(plot_tag_x)
     plt.ylabel(plot_tag_y)
@@ -102,23 +108,23 @@ def plot_single_sweep(data, sweep_tag_1, plot_tag_x='VNA_freqs', plot_tag_y='VNA
         yerr = np.array(yerr)
         plt.fill_between(x, y - yerr, y + yerr, alpha=0.4, ls='--')
     note = f'{plot_tag_y} vs {plot_tag_x}\n sweeping {sweep_tag_1}: {sweep_1_info}'
-    plot_fig(note,inside_plot_flag=inside_plot_flag, timestamp=timestamp)
+    plot_fig(note, inside_plot_flag=inside_plot_flag, timestamp=timestamp)
     return data
 
 
 def plot_double_sweep(data, sweep_tag_1='amp', sweep_tag_2='f', plot_tag_x='amp', plot_tag_y='r_ruox', avgtype=None,
-                      baseline=None, offset=0, save=False, inside_plot_flag = True, timestamp=None):
+                      baseline=None, offset=0, save=False, inside_plot_flag=True, timestamp=None):
     if timestamp is None:
         if 'timestamp' in data.keys():
             timestamp = min(data['timestamp'])
     # filter mask for Double sweep
     sweep1 = data[sweep_tag_1]
-    sweep_1 = get_sweep(data,sweep_tag_1)
-    sweep_1_info = get_sweep_info(data,sweep_tag_1)
-    
+    sweep_1 = get_sweep(data, sweep_tag_1)
+    sweep_1_info = get_sweep_info(data, sweep_tag_1)
+
     sweep2 = data[sweep_tag_2]
-    sweep_2 = get_sweep(data,sweep_tag_2)
-    sweep_2_info = get_sweep_info(data,sweep_tag_2)
+    sweep_2 = get_sweep(data, sweep_tag_2)
+    sweep_2_info = get_sweep_info(data, sweep_tag_2)
 
     if sweep_tag_1 == plot_tag_x:
         sweep_tag = sweep_tag_2
@@ -136,6 +142,7 @@ def plot_double_sweep(data, sweep_tag_1='amp', sweep_tag_2='f', plot_tag_x='amp'
         secondsweep = sweep2
         secondsweep_list = sweep_2
         secondsweep_tag = sweep_tag_2
+
     def get_mask(ssweep, secondssweep):
         return np.array(np.logical_and(sweep == ssweep, secondsweep == secondssweep))
 
@@ -144,20 +151,20 @@ def plot_double_sweep(data, sweep_tag_1='amp', sweep_tag_2='f', plot_tag_x='amp'
     legend = []
     vg = []
     slope = []
-    
+
     if baseline is not None and baseline != 'difference':
-        if isinstance(baseline, float) or isinstance(baseline, int): # baseline is constant
+        if isinstance(baseline, float) or isinstance(baseline, int):  # baseline is constant
             current_mask = sweep == baseline
-            x0, y0 = calc_average_spectrum(data[plot_tag_x][current_mask],data[plot_tag_y][current_mask], type=avgtype)
-        else: # baseline is a dict
+            x0, y0 = calc_average_spectrum(data[plot_tag_x][current_mask], data[plot_tag_y][current_mask], type=avgtype)
+        else:  # baseline is a dict
             x0, y0 = calc_average_spectrum(baseline[plot_tag_x], baseline[plot_tag_y], type=avgtype)
     else:
         y0 = 0
-        
+
     newdata = {}
     for i in range(0, len(sweep_list)):
-        current_mask = sweep==sweep_list[i]
-        x, y = calc_average_spectrum(data[plot_tag_x][current_mask],data[plot_tag_y][current_mask], type=avgtype)
+        current_mask = sweep == sweep_list[i]
+        x, y = calc_average_spectrum(data[plot_tag_x][current_mask], data[plot_tag_y][current_mask], type=avgtype)
         x = np.array(x)
         y = np.array(y)
         if i == 0:
@@ -168,24 +175,26 @@ def plot_double_sweep(data, sweep_tag_1='amp', sweep_tag_2='f', plot_tag_x='amp'
         if baseline == 'difference':
             difference = y - y_previous
         x, difference = filter_nan(x, difference)
-        plt.plot(x, difference + i * offset, ls='-', marker='o', ms=0, mfc='none',c=get_color_cycle(len(sweep_list))[i])
+        plt.plot(x, difference + i * offset, ls='-', marker='o', ms=0, mfc='none',
+                 c=get_color_cycle(len(sweep_list))[i])
         legend += [f'{round(sweep_list[i], 2)}']
         newdata.update({f'{round(sweep_2[i], 2)}': (x, difference)})
         y_previous = y
     plt.xlim(min(x), max(x) + abs(max(x) - min(x)) / 5)
     if save:
         with open('temp', 'wb') as f:
-          pickle.dump(newdata, f)
+            pickle.dump(newdata, f)
     plt.legend(legend)
     plt.xlabel(plot_tag_x)
     plt.ylabel(plot_tag_y)
-    
+
     note = f'{plot_tag_y} vs {plot_tag_x}\n sweeping {sweep_tag_1}: {sweep_1_info}'
-    plot_fig(note,inside_plot_flag=inside_plot_flag, timestamp=timestamp)
+    plot_fig(note, inside_plot_flag=inside_plot_flag, timestamp=timestamp)
     return data
 
 
-def plot_cmap(data, plot_tag_x='VNA_freqs', plot_tag_y='r_ruox', plot_tag_z='VNA_log_mag', avgtype=None, inside_plot_flag = True, timestamp=None):
+def plot_cmap(data, plot_tag_x='VNA_freqs', plot_tag_y='r_ruox', plot_tag_z='VNA_log_mag', avgtype=None,
+              inside_plot_flag=True, timestamp=None):
     if timestamp is None:
         if 'timestamp' in data.keys():
             timestamp = min(data['timestamp'])
@@ -193,15 +202,15 @@ def plot_cmap(data, plot_tag_x='VNA_freqs', plot_tag_y='r_ruox', plot_tag_z='VNA
         fg = plt.figure(figsize=fig_size, dpi=300)
     gs = fg.add_gridspec(1, 2, width_ratios=[0.9, 0.1])
     ax = fg.add_subplot(gs[0])
-    
+
     # filter mask for Double sweep
     sweep1 = data[plot_tag_x]
-    sweep_1 = get_sweep(data,plot_tag_x)
-    sweep_1_info = get_sweep_info(data,plot_tag_x)
-  
+    sweep_1 = get_sweep(data, plot_tag_x)
+    sweep_1_info = get_sweep_info(data, plot_tag_x)
+
     sweep2 = data[plot_tag_y]
-    sweep_2 = get_sweep(data,plot_tag_y)
-    sweep_2_info = get_sweep_info(data,plot_tag_y)
+    sweep_2 = get_sweep(data, plot_tag_y)
+    sweep_2_info = get_sweep_info(data, plot_tag_y)
 
     x = []
     y = []
@@ -225,4 +234,4 @@ def plot_cmap(data, plot_tag_x='VNA_freqs', plot_tag_y='r_ruox', plot_tag_z='VNA
     ax.set_xlabel(plot_tag_x)
     ax.set_ylabel(plot_tag_y)
     note = f'{plot_tag_y} vs {plot_tag_x}\n colormap {plot_tag_z}'
-    plot_fig(note,inside_plot_flag=inside_plot_flag, timestamp=timestamp)
+    plot_fig(note, inside_plot_flag=inside_plot_flag, timestamp=timestamp)
